45453ae66184c5d11039d4b3b61106c678e0aea4
hugo <hugo@astrek.net>
2023-02-03 12:37:26 +0200

    Chapter_04 Templating Engines

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
 const express = require('express')
 const path = require('path')
 const app = new express()
-//const ejs = require('ejs')
-//app.set('view engine', 'ejs')
+const ejs = require('ejs')
+app.set('view engine', 'ejs')
 
 //const mongoose = require('mongoose')
 //mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true}
@@ -14,22 +14,21 @@ app.listen(4000, ()=>{
 })
 
 app.get('/', (req,res)=>{
-    res.sendFile(path.resolve(__dirname, 'pages/index.html'))
-    //res.render('index')
+    res.render('index')
 })
 
 app.get('/about', (req,res)=>{
-//    res.render('about')
-    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
+    res.render('about')
+//    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
 })
 
 app.get('/contact', (req,res)=>{
-//    res.render('contact')
-    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
+    res.render('contact')
+//    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
 })
 
 app.get('/post', (req,res)=>{
-//    res.render('post')
-    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
+    res.render('post')
+//    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
 })
 
