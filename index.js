5790253e62c3b84efc2ef3abec0034a324f34e5f
hugo <hugo@astrek.net>
2023-02-04 09:38:35 +0200

    Chapter_05 Saving Posts to the Database

--- index.js
+++ index.js
@@ -6,8 +6,15 @@ app.set('view engine', 'ejs')
 
 const mongoose = require('mongoose')
 mongoose.connect('mongodb://localhost/my_database', {useNewUrlParser: true})
+mongoose.set('strictQuery', false)
+
+const BlogPost = require('./models/BlogPost.js')
 
 app.use(express.static('public'))
+app.use(express.json())
+app.use(express.urlencoded())
+
+
 
 app.listen(4000, ()=>{
 	console.log('App listening on port 4000')
@@ -19,16 +26,24 @@ app.get('/', (req,res)=>{
 
 app.get('/about', (req,res)=>{
     res.render('about')
-//    res.sendFile(path.resolve(__dirname, 'pages/about.html'))
 })
 
 app.get('/contact', (req,res)=>{
     res.render('contact')
-//    res.sendFile(path.resolve(__dirname, 'pages/contact.html'))
 })
 
 app.get('/post', (req,res)=>{
     res.render('post')
-//    res.sendFile(path.resolve(__dirname, 'pages/post.html'))
 })
 
+app.get('/post/new', (req,res)=>{
+    res.render('create')
+})
+
+app.post('/post/store', async (req,res)=>{
+    // model creates a new doc with browser data
+    console.log(req.body)
+	await BlogPost.create(req.body,(error,blogpost) =>{
+	    res.redirect('/')
+	})    
+})
