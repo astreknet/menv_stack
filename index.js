7accdae098b1d1e1360d39bb34889b171b9bf8f1
hugo <hugo@astrek.net>
2023-02-04 23:28:02 +0200

    Chapter_07 Uploading an Image with Express

--- index.js
+++ index.js
@@ -47,9 +47,15 @@ app.get('/post/:id', async (req,res)=>{
     })
 })
 
+const fileUpload = require('express-fileupload') 
+app.use(fileUpload())
+
 app.post('/post/store', async (req,res)=>{
-    console.log(req.body)
-	await BlogPost.create(req.body)
-	res.redirect('/')
+    let image = req.files.image;  
+	image.mv(path.resolve(__dirname,'public/img',image.name),async (error)=>{
+        console.log(req.body)
+	    await BlogPost.create(req.body)
+	    res.redirect('/')
+    })
 })
 
