6aee95c6a3ec6abb8e234e18b46fed3cfcddc409
hugo <hugo@astrek.net>
2023-02-04 23:39:45 +0200

    Chapter_07 Images to Database

--- index.js
+++ index.js
@@ -54,7 +54,10 @@ app.post('/post/store', async (req,res)=>{
     let image = req.files.image;  
 	image.mv(path.resolve(__dirname,'public/img',image.name),async (error)=>{
         console.log(req.body)
-	    await BlogPost.create(req.body)
+	    await BlogPost.create({
+            ...req.body,
+            image: '/img/' + image.name
+        })
 	    res.redirect('/')
     })
 })
