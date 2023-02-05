998ec1ebb523e8ee928f1134c4e91f6cd38193ac
hugo <hugo@astrek.net>
2023-02-05 22:58:07 +0200

    Chapter_09 MVC Middleware

--- controllers/findPost.js
+++
@@ -1,6 +0,0 @@
-module.exports = (req,res)=>{
-    const blogpost = BlogPost.findById(req.params.id)
-    res.render('post',{
-        blogpost
-    })
-}
--- index.js
+++ index.js
@@ -1,22 +1,15 @@
 const express = require('express')
-const path = require('path')
 const app = new express()
 const mongoose = require('mongoose')
 const ejs = require('ejs')
-const BlogPost = require('./models/BlogPost.js')
 const fileUpload = require('express-fileupload') 
 
-const customMiddleWare = (req,res,next)=>{
-	console.log('Custom middle ware called')
-	next()
-	}
-const validateMiddleWare = (req,res,next)=>{    
-	if(req.files == null || req.body.title == null){        
-	    return res.redirect('/post/new')
-	}    
-	next()
-}
+const customMiddleWare = require("./middleware/customMiddleware")
+const validateMiddleWare = require("./middleware/validateMiddleware")
+const homeController = require('./controllers/home')
 const newPostController = require('./controllers/newPost')
+const storePostController = require('./controllers/storePost')
+const getPostController = require('./controllers/getPost')
 
 app.set('view engine', 'ejs')
 
@@ -34,32 +27,7 @@ app.listen(4000, ()=>{
 	console.log('App listening on port 4000')
 })
 
-app.get('/', async (req,res)=>{
-	const blogposts = await BlogPost.find({})
-	res.render('index',{
-        blogposts
-	});
-    console.log(blogposts)
-})
-
-
+app.get('/',homeController)
 app.get('/post/new', newPostController)
-
-app.get('/post/:id', async (req,res)=>{
-    const blogpost = await BlogPost.findById(req.params.id)
-    res.render('post',{
-        blogpost
-    })
-})
-
-app.post('/post/store', async (req,res)=>{
-    let image = req.files.image;  
-	image.mv(path.resolve(__dirname,'public/img',image.name),async (error)=>{
-        console.log(req.body)
-	    await BlogPost.create({
-            ...req.body,
-            image: '/img/' + image.name
-        })
-	    res.redirect('/')
-    })
-})
+app.get('/post/:id', getPostController)
+app.post('/post/store', storePostController)
