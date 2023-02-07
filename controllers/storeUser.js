3e5e366e943149b03e033997e0584e992a08580f
hugo <hugo@astrek.net>
2023-02-07 21:09:43 +0200

    Chapter_10 validation, login process

--- controllers/storeUser.js
+++ controllers/storeUser.js
@@ -1,8 +1,11 @@
 const User = require('../models/User.js')
-    const path = require('path')
+const path = require('path')
 	 
 module.exports = (req,res)=>{ 
 	User.create(req.body, (error, user) => {
+        if(error){
+            return res.redirect('/auth/register')
+        }
         res.redirect('/')        
 	})               
 }
