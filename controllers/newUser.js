788d22f7953ec3f5c81c30d89b93698f583a27bc
hugo <hugo@astrek.net>
2023-02-09 17:59:01 +0200

    Chapter 12 Error messages, persist data

--- controllers/newUser.js
+++ controllers/newUser.js
@@ -1,6 +1,16 @@
 module.exports = (req, res) =>{
-	res.render('register', {
-        errors: req.flash('validationErrors')
+	var username = "" 
+	var password = ""
+	const data = req.flash('data')[0];    
+	 
+	if(typeof data != "undefined"){        
+	    username = data.username
+	    password = data.password
+	}
+    res.render('register', {
+        errors: req.flash('validationErrors'),
+        username: username,
+        password: password
         //errors: req.session.validationErrors
     }) // render register.ejs
 }
